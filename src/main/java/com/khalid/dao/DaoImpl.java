16a33a205ddb9f871c2fe1fb69b1c35e0bbbcf6e
khalidmk1 <mkadmikh12@gmail.com>
2025-03-21 12:17:47 +0000

    first commit

---
+++ src/main/java/com/khalid/dao/DaoImpl.java
@@ -0,0 +1,8 @@
+package com.khalid.dao;
+
+public class DaoImpl implements IDao {
+    @Override
+    public double getData() {
+        return 0;
+    }
+}
---
+++ src/main/java/com/khalid/dao/IDao.java
@@ -0,0 +1,5 @@
+package com.khalid.dao;
+
+public interface IDao {
+    double getData();
+}
